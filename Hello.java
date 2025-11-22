d06d03cd88d6016f10045173347d5cd4ac39027e
Arjun Kumar <kalamoluarjunkumar@gmail.com>
2025-11-22 18:45:48 +0530

    Greetings Mr.Arjun Kumar Kalamolu

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Hi,Arjun How Are You ?");
+        System.out.println("Goin Somewhere ?");
     }
 }
--- src/Reply.java
+++ src/Reply.java
@@ -3,6 +3,6 @@ package src;
 public class Reply {
     public static void main(String[] args){
         System.out.println("I'm Fine,What about you ?");
-        System.out.println("I heard you did land a tech job in a product based company");
+        System.out.println("I heard you did land a tech job in a product based company,is it true ?");
     }
 }

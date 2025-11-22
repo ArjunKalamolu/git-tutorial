862ad1a3dfc222ec473ab0eb7332d90a427a7184
Arjun Kumar <kalamoluarjunkumar@gmail.com>
2025-11-22 19:51:47 +0530

    Greeting to Arjun Kumar

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
     public static void main(String[] args){
-        System.out.println("Hi,Arjun How Are You ?");
-        System.out.println("Goin Somewhere ?");
+        System.out.println("Hi,Arjun");
+        System.out.println("Hii,My Friend");
     }
 }
--- src/Reply.java
+++ src/Reply.java
@@ -3,6 +3,5 @@ package src;
 public class Reply {
     public static void main(String[] args){
         System.out.println("I'm Fine,What about you ?");
-        System.out.println("I heard you did land a tech job in a product based company,is it true ?");
     }
 }

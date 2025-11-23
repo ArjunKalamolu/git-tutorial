e22fa00bdb2b48a2d41cca2778846b479d6045c8
Arjun Kumar <kalamoluarjunkumar@gmail.com>
2025-11-23 06:36:43 +0530

    Modified Formalities As per Time

--- Formality.java
+++ Formality.java
@@ -1,6 +1,6 @@
 public class Formality {
     public static void main(String[] args){
-        System.out.println("Sorry,I forgot to add Mr beside Arjun");
+        System.out.println("Sorry,I forgot to add Mr beside the name Arjun");
     }
     
 }

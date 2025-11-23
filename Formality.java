25fc624c27d1233ffee8e6e2a36aa66ade1fb87d
Arjun Kumar <kalamoluarjunkumar@gmail.com>
2025-11-23 07:23:51 +0530

    New Git Repository

--- Formality.java
+++ Formality.java
@@ -3,6 +3,7 @@ public class Formality {
         System.out.println("Sai : Sorry,I forgot to add Mr beside the name Arjun");
         System.out.println("Sai : Are you on job hunt ?");
         System.out.println("Arjun : Yes.I'm");
+        System.out.print(" "+"What about you ? you too doing the same..");
     }
     
 }
--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Hi,Arjun");
+        System.out.print(" "+"How do you do ? Man..");
         
     }
 }

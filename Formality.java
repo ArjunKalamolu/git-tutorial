7aa0c83a96dfdc168062e8204bad2a165505d23d
Arjun Kumar <kalamoluarjunkumar@gmail.com>
2025-11-23 06:44:41 +0530

    Long time no see my friend

--- Formality.java
+++ Formality.java
@@ -1,6 +1,8 @@
 public class Formality {
     public static void main(String[] args){
-        System.out.println("Sorry,I forgot to add Mr beside the name Arjun");
+        System.out.println("Sai : Sorry,I forgot to add Mr beside the name Arjun");
+        System.out.println("Sai : Are you on job hunt ?");
+        System.out.println("Arjun : Yes.I'm");
     }
     
 }

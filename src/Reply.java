4b4c1ba80046d9106a89b3aadf699d7807a118a6
Arjun Kumar <kalamoluarjunkumar@gmail.com>
2025-11-23 06:13:42 +0530

    Added Extensions To Greetings

--- src/Reply.java
+++ src/Reply.java
@@ -2,6 +2,6 @@ package src;
 
 public class Reply {
     public static void main(String[] args){
-        System.out.println("I'm Fine,What about you ?");
+        System.out.println("I'm Fine,What about you ? Going somewhere");
     }
 }
